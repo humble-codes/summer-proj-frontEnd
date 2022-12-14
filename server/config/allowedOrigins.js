f0cba5040cc047a453c4c31348bd6daa6eee843e
humble-codes <sprudra5001@outlook.com>
2022-12-14 22:33:10 +0530

    cors-update3

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -5,7 +5,9 @@ const allowedOrigins = [
   'https://project-mgmnt-systm.cyclic.app',
   'https://www.project-mgmnt-systm.cyclic.app',
   'https://project-mgmnt-systm.netlify.app',
-  'https://project-mgmnt-systm.netlify.app',
+  'https://www.project-mgmnt-systm.netlify.app',
+  'https://project-mgmnt-systm.onrender.com',
+  'https://www.project-mgmnt-systm.onrender.com/',
   'https://lucasconstantino.github.io'
 ]
 
