92132ab0c6580f0012e53ebb2b41a7fd4a5ee411
humble-codes <sprudra5001@outlook.com>
2022-12-14 22:18:22 +0530

    cors-updste 2

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   'https://proj-mgmt-sys.onrender.com',
   'https://www.proj-mgmt-sys.onrender.com',
-  // 'http://localhost:3000',
+  'http://localhost:3000',
   'https://project-mgmnt-systm.cyclic.app',
   'https://www.project-mgmnt-systm.cyclic.app',
   'https://project-mgmnt-systm.netlify.app',
