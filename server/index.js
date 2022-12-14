257daf6203c6db1d1a245611d6ecc77adbdc7f77
humble-codes <sprudra5001@outlook.com>
2022-12-14 22:56:58 +0530

    updates-app.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const colors = require('colors');
 const cors = require('cors');
@@ -33,6 +34,16 @@ app.use(
     graphiql: process.env.NODE_ENV === 'development',
   })
 );
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 connectDB().then(() => {
   app.listen(port, () => {
