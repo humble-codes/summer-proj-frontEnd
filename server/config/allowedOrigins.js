6be8f2f8d5f97075c1b6230d328139054dd59c12
humble-codes <sprudra5001@outlook.com>
2022-12-14 22:11:55 +0530

    cors-updated

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,9 +1,11 @@
 const allowedOrigins = [
   'https://proj-mgmt-sys.onrender.com',
   'https://www.proj-mgmt-sys.onrender.com',
-  'http://localhost:3000',
-  'https://project-mgmnt-systm.cyclic.app/graphql',
-  'https://www.project-mgmnt-systm.cyclic.app/graphql',
+  // 'http://localhost:3000',
+  'https://project-mgmnt-systm.cyclic.app',
+  'https://www.project-mgmnt-systm.cyclic.app',
+  'https://project-mgmnt-systm.netlify.app',
+  'https://project-mgmnt-systm.netlify.app',
   'https://lucasconstantino.github.io'
 ]
 
